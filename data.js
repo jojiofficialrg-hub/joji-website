688074e082efe4fe98c94cb604f2f8587c052fee
jojiofficialrg-hub <jojiofficial.rg@gmail.com>
2026-02-03 12:39:42 +0530

    Add files via upload

---
+++ data.js
@@ -0,0 +1,13 @@
+const products = [
+  {
+    name: "Bridal Lehenga",
+    fabric: "Silk with Maggam Work",
+    colors: [
+      { name: "Red", available: true },
+      { name: "Maroon", available: false },
+      { name: "Bottle Green", available: true }
+    ],
+    offer: "10% OFF – Custom Order",
+    measurement: "Bust, Waist, Length (EN + HI + TE)"
+  }
+];
